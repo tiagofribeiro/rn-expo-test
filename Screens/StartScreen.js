138e034e80d27e7af5d9d3b52f3db7d0b667734e
Tiago <tiago.ribeiro@sette.inf.br>
2020-01-07 17:47:53 -0300

    Separação de componentes

--- Screens/StartScreen.js
+++ Screens/StartScreen.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
+import Card from '../Components/Card';
+
 const StartScreen = props => {
     return (
         <View style={styles.sreen}>
             <Text style={styles.title}>Iniciar Jogo</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Escolha um número</Text>
                 <TextInput />
                 <View style={styles.buttonContainer}>
                     <Button title="Reset" onPress={() => { }} />
                     <Button title="Confirmar" onPress={() => { }} />
                 </View>
-            </View>
+            </Card>
         </View>
     );
 };
@@ -30,12 +32,7 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        backgroundColor: 'white'
+        alignItems: 'center'
     },
     buttonContainer: {
         flexDirection: 'row',
